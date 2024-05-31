a443aa112b29b5b8aee755cbcc4b8a8a6644825a
sasu-y <116713900+sasu-y@users.noreply.github.com>
2024-05-31 13:50:52 +0900

    Create test_fizzbuzz.py

---
+++ test_fizzbuzz.py
@@ -0,0 +1,7 @@
+import unittest
+import fizzbuzz
+class TestFizzBuzz(unittest.TestCase):
+def test_fizz(self):
+self.assertEqual('Fizz', fizzbuzz.fizzbuzz(3))
+def test_buzz(self):
+self.assertEqual('Buzz', fizzbuzz.fizzbuzz(5))
